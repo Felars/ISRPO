22c94741817bbb8d7e9b742949681b8c739ab092
Артур Фельдбейн <artur.feldbeyn100@gmail.com>
2021-06-28 12:27:19 +0700

    Готово

---
+++ ISRPO/I.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISRPO
+{
+    public class I
+    {
+        public bool Insert(string namee, string price, string quantity, string sum)
+        {
+            DBEntities db = new DBEntities();
+            try
+            {
+                Cash cash = new Cash();
+
+                cash.Namee = namee;
+                cash.Price = Convert.ToDecimal(price);
+                cash.Quantity = quantity;
+                cash.Sum = Convert.ToDecimal(sum);
+                db.Cash.Add(cash);
+                db.SaveChanges();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- ISRPO/MainWindow.xaml.cs
+++ ISRPO/MainWindow.xaml.cs
@@ -20,9 +20,27 @@ namespace ISRPO
     /// </summary>
     public partial class MainWindow : Window
     {
+        DBEntities db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new DBEntities();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            db = new DBEntities();
+            dgogrenci3.ItemsSource = db.Cash.ToList();
+        }
+
+        private void ADD1(object sender, RoutedEventArgs e)
+        {
+            I Insert = new I();
+            Insert.Insert(Namee.Text, Price.Text, Quantity.Text, Sum.Text);
+            dgogrenci3.ItemsSource = db.Cash.ToList();
+            Namee.Clear();
+            Price.Clear();
+            Quantity.Clear();
+            Sum.Clear();
         }
     }
 }
